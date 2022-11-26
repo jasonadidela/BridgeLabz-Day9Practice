6f63e94cdc9c6b725a46daa1a1f8854d3c130221
jasonadidela <jasonadidela@gmail.com>
2022-11-26 09:18:21 +0530

    Added code for Person class on main Branch

--- Address Book Management System Development/src/AddressBookMain.java
+++ Address Book Management System Development/src/AddressBookMain.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 public class AddressBookMain {
-    public static void main(String[] args) {
+    public static void main() {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter The following number:\n 1. To Display Add Contact:\n 2. To Display Edit Contact:" +
+                                    "\n 3. To Display Delete Contact: ");
+        int num = scanner.nextInt();
         Person person = new Person();
-        System.out.println("Person Contact Details");
-        CreateAddressBook.addContact(person);
-        System.out.println("Person Contact Details After edited");
-        //CreateAddressBook.editContact(person);
+        switch (num) {
+
+            case 1 -> {
+                System.out.println("Person Contact Details");
+                CreateAddressBook.addContact(person);
+            }
+            case 2 -> {
+                System.out.println("Person Contact Details After edited");
+                CreateAddressBook.editContact(person);
+            }
+            case 0 -> {
+                System.out.println("Person Contact Deleted");
+                CreateAddressBook.deleteContact(person);
+            }
+        }
     }
 }
--- Address Book Management System Development/src/CreateAddressBook.java
+++ Address Book Management System Development/src/CreateAddressBook.java
@@ -11,7 +11,8 @@ public class CreateAddressBook {
             person.setEmail("email");
 
         }
-        static void editContact(Person person) {
+        static void editContact(Person person){
+
             person.setFirstName("FIRSTNAME");
             person.setLastName("LASTNAME");
             person.setAddress("ADDRESS");
@@ -20,6 +21,18 @@ public class CreateAddressBook {
             person.setZipCode("ZIPCODE");
             person.setPhoneNumber("NUMBER");
             person.setEmail("EMAIL");
+
+    }
+
+        static void deleteContact(Person person) {
+            person.setFirstName("Null");
+            person.setLastName("Null");
+            person.setAddress("Null");
+            person.setCity("Null");
+            person.setState("Null");
+            person.setZipCode("Null");
+            person.setPhoneNumber("Null");
+            person.setEmail("Null");
 
     }
 
